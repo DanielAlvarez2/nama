628a7d1447531ca08d48cd70a8a758844cc53c3e
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-06-26 11:14:58 -0400

    sake modal is responsive

--- app/sake/page.jsx
+++ app/sake/page.jsx
@@ -1,10 +1,67 @@
+'use client'
 import Link from 'next/link'
+import { AiTwotoneCloseCircle } from "react-icons/ai";
 
 export default function Sake(){
-    return(
-    <div className="webpage">
 
-      <div style={{width:'100%',textAlign:'center',marginTop:'15px'}}>
+    function openModal(section,producer,bin,size,price,description,image){
+        document.querySelector('#sake-modal').style.display = 'grid'
+        document.querySelector('#sake-modal-section').innerText = section
+        document.querySelector('#sake-modal-producer-name').innerText = producer
+        document.querySelector('.sake-modal-bin-left').innerText = bin
+        document.querySelector('.sake-modal-bin-mobile').innerText = bin
+        document.querySelector('.sake-modal-size').innerText = size
+        document.querySelector('.sake-modal-price').innerText = price
+        document.querySelector('.sake-modal-description').innerText = description
+        document.querySelector('#sake-modal-image').src = image
+
+    }
+    function closeModal(){
+        document.querySelector('#sake-modal').style.display = 'none'
+    }
+
+    return(
+    <div className="webpage sake-webpage">
+
+        <div id='sake-modal'>
+            <div id='sake-modal-content' style={{width:'6in'}}>
+                <AiTwotoneCloseCircle   size='30'
+                                        style={{cursor:'pointer',
+                                                position:'fixed',
+                                                top:'0',
+                                                right:'0'
+                                        }} 
+                                        onClick={closeModal} />
+                <div style={{width:'100%',textAlign:'center'}}>
+                    <img id='sake-modal-image' />
+                </div>
+                <div id='sake-modal-info'>
+                    <h2 style={{width:'100%',textAlign:'center',marginBottom:'0px'}}>SAKE</h2>
+                    <div className='sake-section'>
+                        <h3 id='sake-modal-section'></h3>
+                            <div className='sake-producer'>    
+                                <div className='sake-producer-name'><span className='bin-left'></span>
+                                    <span id='sake-modal-producer-name'></span>
+                                </div>{/* .sake-producer-name */}
+                                <div className='sake'>
+                                    <div className='sake-flexbox-left'>
+                                        <span className='bin-left sake-modal-bin-left'></span>
+                                        <span className='sake-description sake-modal-description'></span>
+                                    </div>{/* .sake-flexbox-left */}
+                                    <div className='sake-flexbox-right'>
+                                        <span className='bin-mobile sake-modal-bin-mobile'></span>
+                                        <span className='sake-size sake-modal-size'></span>
+                                        <span className='sake-price sake-modal-price'></span>
+                                    </div>{/* .sake-flexbox-right */}
+                                </div>{/* .sake */}
+                            </div>{/* .sake-producer */}
+                    </div>{/* .sake-section */}
+
+                </div>{/* .sake-modal-info */}
+            </div>{/* .sake-modal-content */}
+        </div>{/* .sake-modal */}
+
+      <div style={{width:'100%',textAlign:'center',paddingTop:'15px'}}>
         <svg xmlns="http://www.w3.org/2000/svg" width="120" height="60">
           <path fill="#281E09" d="m51.9 30.5-.4-1c-.9.5-1 1.6-1.6 2.2-1.3 1.4-4.3 1.3-6 1.3-.6 3.3-.5 6.6-.8 10-.3 3.4-1.8 9-1 12.4.1.5.5.8.8 1.2l-.6.6.6 1.8c-.8.2-1-1.3-1.2-1.8a74.1 74.1 0 0 1 1-35.5c.6-2.3 1.5-4.4 2-6.7-1.8 1.5-3.7 2.8-5.4 4.4-2.3 2-6.3 5.1-8.1 7.2-1 1.1-1.9 2.8-3 4-.6.7-2 2.3-2.7 2.5-3.4 1.1-7.8-2.9-9.7-5.3l-6.7 5.7L9 33l-3.2 2.3c-.8.4.1-1-.6-.6-.9.5-1.2 2-2.5 1.2-.2.9 1.2.7 1.2 1-1 1.5-3.8 2-3.8-.3 0-3.3 9.2-10.1 11.4-12.7 1-.4 2.6.5 3.5.5.7 0 1.4-.4 2.2-.2 1.5.4 4.5 3.4 6.2 4 2 .6 2.8-.3 4.3-1.4 2.5-1.6 4.8-3.8 7.3-5.5 2-1.5 9.2-5.4 10.3-7.4 1.7-3 2.1-8.6 3.6-12 .2-.5.4-1 1-1.4 1.7 4.8.7 10.4 1 15.5 0 2.4.1 6.1.5 8.4.2.8 1 2.1 1 2.3 0 .2-.4.6-.4 1.2-.1 1.2.3 2.3.3 3.5v3.5c0 .3.2 1-.3 1-.2-1.7.2-3.8 0-5.4Zm-.9-5.4L49.7 7.2c-.7 1.2-1 2.7-1.5 4l-.6 2.3c-1.2 4.2-2 8.5-3 12.8 2.3-.6 4-1.3 6.4-1.2Zm34.5 5.1c.3-2.7 1.6-5.6 2-8.2l-3.8 2c-1.3 1.9-6.3 5-8.4 4.9-2.1-.1-4-1.6-5.8-2.3-2.2 1.2-7.4 10-10.2 5.8-.4-.6-1.4-3.3-1.3-4 0-1 2.6-2.2 3.3-3.2l.3.5 5.9-5.7c5.5.1 5.4 5.8 11.7 3.4 2-.8 8.6-5 10.2-4.8 2.2.2 0 2.7-.3 3.4l-1.6 5-2 7.7v-4.5Zm27.3-6.2 7.2.6c0 .5-.7.3-1 .3-.8 0-1.5-.3-2-.3-1.3 0-2.6.5-4 .3-.3.2 2.4 6.2 2.4 7.4 0 .4-1.3 3.8-1.6 3.8-.6-.2-.6-1-.9-1.3-.3-.4-1-.6-1.4-1.3-.8-1.3-2.3-8.2-2.7-8.4-1.5.1-3 .4-4.5.7-1.3.3-6.1 1.2-7 1.7-.4.2-.5 1-1 1.3-.7.3-1.6 0-2.1.3-.4.2-.9 3.7-2.6 3.6-.4-.5 1.6-3.8 1.6-4.5.1-.8-.5-1.9 0-2.8.3-.5 2.6-.7 3-.9.4-.2 3.4-4 4.4-4.7 1.5-1.3 3-2.8 5.2-2.5.6-1.7.2-4.5 3-3.7 1.7.5 3.3 8.5 4 10.4Zm-7.3-5-.1-1a19.6 19.6 0 0 0-7.1 5.8h1.4l-.9 1 9.6-1.1-1-5-1.9.4Z"></path>
         </svg>
@@ -35,7 +92,14 @@ export default function Sake(){
                         <div className='sake-producer-name'><span className='bin-left'></span>
                         Masumi
                         </div>{/* .sake-producer-name */}
-                        <div className='sake'>
+                        <div className='sake' onClick={()=>openModal(   'SPARKLING',
+                                                                        'Masumi',
+                                                                        '3001',
+                                                                        '750 ml',
+                                                                        '425',
+                                                                        'Masumi Sparkling Grand Prix, Nagano',
+                                                                        '/masumi.jpg'
+                                                        )}>
                             <div className='sake-flexbox-left'>
                                 <span className='bin-left'>3001</span>
                                 <span className='sake-description'>Masumi Sparkling Grand Prix, Nagano</span>

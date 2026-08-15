77764076c3a10029267c628dbef04d6d833a99e5
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-08-14 23:30:19 -0400

    make first drink page .small-paper to make responsive

--- app/drinks/page.jsx
+++ app/drinks/page.jsx
@@ -21,7 +21,7 @@ export default function DrinksMenu(){
       <Navbar page='drinks' />
       
     
-      <div  className="drink-menu-page small-paper" 
+      <div  className="small-paper" 
             style={{
                     
                     // backgroundImage:'url("akari-drinks-1.jpg")',
